d00d95d3e4b8e3feb3a26c81c1fa0b850ba96ec2
DEN41R <145060707+DEN41R@users.noreply.github.com>
2026-02-14 21:39:41 +0300

    Add files via upload

---
+++ modpack/tutorial.bat
@@ -0,0 +1,59 @@
+@echo off
+cls
+title CarX Street 1.6.0 Modpack Tutorial
+color 3
+
+echo.
+echo ===================================================
+echo         CarX Street 1.6.0 Modpack Tutorial
+echo ===================================================
+echo.
+
+echo [1] INSTALLATION
+echo ================
+echo.
+echo 1. Install the 1.6.0 version of the game:
+echo    - Open Steam
+echo    - Press Win+R
+echo    - Type: steam://open/console
+echo    - In console paste this command:
+echo      download_depot 1114150 1114151 6995232317792283507
+echo.
+
+echo 2. Copy these files to your game folder:
+echo    - Catalog to: \CarX Street_Data\StreamingAssets\aa
+echo    - 47c bundle to: \CarX Street_Data\StreamingAssets\aa\StandaloneWindows64
+echo.
+
+echo 3. [OPTIONAL] Save file location:
+echo    C:\Program Files (x86)\Steam\userdata\YOUR_STEAM_ID\1114150\remote
+echo.
+
+echo [2] TROUBLESHOOTING
+echo ===================
+echo.
+echo 1. What should I do if my game doesn't load after 28?%
+echo The answer is that your save file has stuff from version 1.9. Use the save file from the modpack using method 3.
+echo.
+echo 2. The game does not load beyond 64%
+echo The answer is that you have not migrated the Catalog.json in the right place
+echo.
+
+echo [3] MODPACK FEATURES
+echo ====================
+echo.
+echo 1. Ready-made files for the game
+echo 2. Invisible parts for each machine
+echo 3. Free cars at the dealership and without DLC or event type requirements
+echo 4. The ability to change the characteristics of cars more than the game allows 
+echo 5. You can also now modify the file if necessary or ask me.
+echo.
+
+echo ===================================================
+echo             Enjoy your game!
+echo ===================================================
+echo.
+echo Last Update This Bundle - 07.11.25
+echo I should update it already and add unreleased all engines and transsmision and etc stuff.
+echo.
+pause
